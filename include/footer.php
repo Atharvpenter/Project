8bff905e4346203daa75f036a3a9dc75a2b61d14
Atharvpenter <atharvpenter65@gmail.com>
2025-11-20 15:33:50 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,53 @@
+<footer class="full-row bg-secondary p-0">
+            <div class="container">
+                <div  class="row">
+                    <div class="col-lg-12">
+                        <div class="divider py-40">
+                            <div class="row">
+                                <div class="col-md-12 col-lg-4">
+                                    <div class="footer-widget mb-4">
+                                        <div class="footer-logo mb-4"> <a href="#"><img class="logo-bottom" src="images/logo/realestate.jpg" alt="image"></a> </div>
+                                        <p class="pb-20 text-white"></p>
+										
+                                        </div>
+                                </div>
+                                <div class="col-md-12 col-lg-8">
+                                    <div class="row">
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">SUPPORT</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="contact.php" class="text-white">CONTACT</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget footer-nav mb-4">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">QUICK LINKS</h4>
+                                                <ul class="hover-text-primary">
+                                                    <li><a href="about.php" class="text-white">ABOUT US</a></li>
+                                                    <li><a href="agent.php" class="text-white">OUR AGENTS</a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4 col-lg-4">
+                                            <div class="footer-widget">
+                                                <h4 class="widget-title text-white double-down-line-left position-relative">CONTACT US</h4>
+                                                <ul class="text-white">
+                                                    <li class="hover-text-primary"><i class="fas fa-map-marker-alt text-white mr-2 font-13 mt-1"></i>Sangamner, Ahilyanagar, Maharastra, 422605</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+91 7083534353</li>
+                                                    <li class="hover-text-primary"> <i class="fas fa-phone-alt text-white mr-2 font-13 mt-1"></i>+91 7385709106</li>
+							                                             <li class="hover-text-primary"><i class="fas fa-envelope text-white mr-2 font-13 mt-1"></i>atharv@realestatecompany.com</li>
+                                                                         <li>abhishek@realestatecompany.com</li>
+                                                </ul>
+                                            </div>
+                                            <div class="footer-widget media-widget mt-4 text-white hover-text-primary"> <a href="#"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-twitter"></i></a> <a href="#"><i class="fab fa-google-plus-g"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="#"><i class="fas fa-rss"></i></a> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+             
+        </footer>
